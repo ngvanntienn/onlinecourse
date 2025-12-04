d36ccf94a3499ad9b38ca0435b850842106009a1
pthao0403 <pthao0403@example.com>
2025-12-04 23:04:52 +0700

    onlinecourse

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+class Database {
+    private $host = '127.0.0.1';
+    private $db = 'onlinecourse'; 
+    private $user = 'root';
+    private $pass = '';  
+    private $charset = 'utf8mb4';
+    public $pdo = null;
+
+    public function __construct() {
+        $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+        try {
+            $this->pdo = new PDO($dsn, $this->user, $this->pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ]);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+}
+?>
